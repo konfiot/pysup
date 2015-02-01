5c5d819a516e07e4273523585caa55e65c329835
konfiot <arthurtoussaint@free.fr>
2015-02-01 16:33:42 +0100

    On avance bien, implémentation du produit, de la somme, et de la crétion par racines

--- polynoms.py
+++ polynoms.py
@@ -1,20 +1,47 @@
 class Polynom():
-	self.coefs = []
-	def __init__(self, coefs):
-		self.coefs = coefs
-	
-	def __init__(self, maincoef, roots):
-		sum = 0
-		nfact = math.factorial(len(roots))
-		for coef in range(len(roots)): # Pour chaque coefficient, on va calculer les relations coefficients racines
-			for i in nfact//(math.factorial(coef)*math.factorial(len(roots)-coef)):
-				
-				
-			
+	def __init__(self, coefs, maincoef = False):
+		if maincoef :
+			self.coefs=[maincoef]
+			for root in coefs:
+				print(root)
+				self *= Polynom([-root,1])
+				print(self)
+		else : self.coefs = coefs
 
 	def __add__(self, other):
-		return Polynom([self[x]+other[x] for x in range(len(self.coefs))])
+		return Polynom([self[x]+other[x] for x in range(max(self.deg()-1, other.deg()-1))])
+	
+	def __iadd__(self, other):
+		while self.deg() < other.deg():
+			self.coefs.append(0)
+		for x in range(self.deg()-1):
+			self[x] += other[x]
+		return self
 
 	def __getitem__(self, i):
-		return self.coefs[i]
+		if i > self.deg() : return 0
+		else : return self.coefs[i]
 
+	def deg(self):
+		return len(self.coefs)-1 # TODO : Eliminer les coefficients nuls
+
+	def __mul__(self,other):
+		coefs = [0]*(self.deg() + other.deg()+1)
+		for k in range(self.deg() + other.deg()+1):
+			for i in range(k+1):
+				coefs[k] += self[i]*other[k-i]
+		return Polynom(coefs)
+
+	def __imul__(self,other):
+		coefs = [0]*(self.deg() + other.deg()+1)
+		for k in range(self.deg() + other.deg()+1):
+			for i in range(k+1):
+				coefs[k] += self[i]*other[k-i]
+		self.coefs = coefs	
+		return self
+
+	def __str__(self):
+		out = ""
+		for i in range(self.deg()+1):
+			out += str(self[i]) + "X^"+str(i) + " + "
+		return out
