f7c03064ae8554c18e131623df45c86da4bf8e01
konfiot <arthurtoussaint@free.fr>
2015-02-02 18:58:48 +0100

    Algo d'euclide

---
+++ misc.py
@@ -0,0 +1,12 @@
+def euclide(a,b):
+	r0 = max(a,b)
+	r1 = min(a,b)
+	xn,xn1 = 0,1
+	yn,yn1 = 1,0
+
+	while r0%r1 != 0:
+		xn,xn1 = xn1-(r0//r1)*xn,xn
+		yn,yn1 = yn1-(r0//r1)*yn,yn
+		r0,r1 = r1, r0%r1 
+
+	return r1,xn,yn
