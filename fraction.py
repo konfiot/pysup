780abc976f9466e9747c62bbb3ef64c21a52449d
konfiot <arthurtoussaint@free.fr>
2015-02-09 18:25:31 +0100

    Egalités et inégalités

--- fraction.py
+++ fraction.py
@@ -11,3 +11,17 @@ class Fraction():
 
 	def __sub__(A,B):
 		return Fraction(A.num*B.den - A.den+B.num,A.den*B.den)
+
+	def __div__(A,B):
+		return Fraction(A.num*B.den,A.den*B.num)
+
+	def __eq__(A,B):
+		return A.num*B.den == A.den*B.num
+
+	def __lt__(A,B):
+		diff = A-B
+		return (diff.num > 0)^(diff.den > 0)
+
+	def __gt__(A,B):
+		return not A < B
+
